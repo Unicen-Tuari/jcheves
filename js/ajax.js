117cdc093c8dded3598fcbc2ea01d9fb2ae158d7
Jesus Cheves <jesuscheves1991@gmail.com>
2014-11-22 17:08:43 -0300

    closes #26 Agrego la funcionalidad para que al hacer click en un articulo, este se agregue al carrito por ajax

--- js/ajax.js
+++ js/ajax.js
@@ -1,13 +1,34 @@
-$("#form_buscar").submit(function(){
+//Ajax de la busqueda del catalogo
+	$(document).ready(function(){
 
-	$.ajax({
-		url	: "index.php",
-		type:"post",
-		data	: $("#form_buscar").serialize(),
-		success : function(data){
-			$("#resultado_busqueda").html(data);
+			$("#form_buscar").submit(function(){
+				$.ajax({
+					url	: "index.php?action=buscar",
+					type:"post",
+					data	: $("#form_buscar").serialize(),
+					success : function(data){
+						$("#resultado_busqueda").html(data);
+
+					}
+
+				});
+				return false;	
+			});
 		}
+	);
 
-	});
-		
-});
+	function clickComprar(idProducto){
+		$.ajax({
+			url  : "index.php",		//URL a la que le pegamos
+			type : "post",			//metodo que usamos para enviar
+			data : {				// data = objeto tipo JSON (CLAVE : VALOR)
+					action: "agregarProductoCarrito", //action ->
+					idProducto:idProducto				
+					},
+
+			success: function(data){ 	// que hace si el envio del JSON fue exitoso
+			}
+		});
+	}
+
+//
