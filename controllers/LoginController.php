d099456cf4fd65e32442fea6059b0708619673e1
Jesus Cheves <jesuscheves1991@gmail.com>
2014-11-22 16:39:55 -0300

    closes #16 Controlador del Login Actualizado para que loguee

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -1,9 +1,37 @@
 <?php
 	class LoginController {
-		public function actionLogin(){
-			include "./views/LoginView.php";
+		public function actionmostrarLogin(){
+			include_once "./views/LoginView.php";
 			$view = new LoginView;
 			$view->render();
+		}
+
+		public function actiondataLogin(){
+			
+			include_once "./models/modelo_usuario.php";
+			$usuario =$_REQUEST['username'];
+			$password =$_REQUEST['password'];
+
+			$busquedaUsuario = new usuario;
+			$resultado = $busquedaUsuario -> buscar_usuario_clave($usuario, $password);
+			if ($resultado != null){
+				$_SESSION['idusuario'] = $resultado[0]['idusuario'];
+				$_SESSION['nombre'] = $usuario;
+				$_SESSION['admin'] = $resultado[0]['admin'];
+				include_once "./controllers/Headernavcontroller.php";
+				$headernavcontroller = new HeaderNavController;
+				$headernavcontroller->actionmostrarheadernav();
+				include_once "./controllers/IndexController.php";
+				$controller = new IndexController();
+				$controller->actionIndex();
+			}
+
+			else{
+				echo "<script type=\"text/javascript\">alert(\"Usuario o clave erroneos\");</script>";
+				include_once "./controllers/LoginController.php";
+				$controller = new LoginController();
+				$controller->actionmostrarLogin();
+			}
 		}
 	}
 ?>
